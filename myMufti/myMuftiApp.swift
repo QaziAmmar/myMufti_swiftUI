794c21e02df2149009b492b53e37ac2a2590d004
Qazi Ammar Arshad <qaziammar.g@gmail.com>
2022-06-20 16:14:08 +0500

    Initial Commit

---
+++ myMufti/myMuftiApp.swift
@@ -0,0 +1,17 @@
+//
+//  myMuftiApp.swift
+//  myMufti
+//
+//  Created by Qazi Ammar Arshad on 20/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct myMuftiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
